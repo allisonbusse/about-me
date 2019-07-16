39774d33e95585a0bccfbc3a1f9cce3863606822
allisonbusse <allisonbusse@gmail.com>
2019-07-16 16:32:14 -0700

    added files and initial design

--- test/tests.js
+++ test/tests.js
@@ -1,29 +1,3 @@
-import isYes from '../src/is-yes.js';
+import './isyes.test.js';
 
-const test = QUnit.test;
 
-test('yes is yes', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
-    const text = 'yes';
-    const expected = true;
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const result = isYes(text);
-
-    //Assert
-    assert.equal(result, expected);
-});
-
-test('no is no', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
-    const text = 'no';
-    const expected = false;
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const result = isYes(text);
-
-    //Assert
-    assert.equal(result, expected);
-});
