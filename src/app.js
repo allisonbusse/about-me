df424217f630cbf9b4d17dbe38dba1d4fcbf66ae
allisonbusse <allisonbusse@gmail.com>
2019-07-16 10:45:26 -0700

    more css fixes

--- src/app.js
+++ src/app.js
@@ -13,6 +13,12 @@ const results = document.getElementById('results');
 startQuiz.onclick = function() {
     //ask user for their name
     const name = prompt('Hi there! What\'s your name?');
+    if(name === null) {
+        return prompt('Come on, we need your name!');
+    }
+    else if(name === '') {
+        return prompt('Please tell us!');
+    }
 
     //confirm they want to take the quiz
     const check = confirm('Hi ' + name + '! Are you sure you\'re ready for this?');
@@ -56,16 +62,19 @@ startQuiz.onclick = function() {
     //return results for 0 or 1 correct answers
     if(correctAnswers < 35) {
         results.textContent = 'Hey ' + name + ', you scored ' + correctAnswers + '% on the quiz! You might want to try again';
+        results.classList.add('bad-score');
     } 
 
     //return results for 2 correct answers
-    if(correctAnswers < 68) {
+    if(correctAnswers === 67) {
         results.textContent = 'Hey ' + name + ', you scored ' + correctAnswers + '% on the quiz! Not bad!';
+        results.classList.add('middle-score');
     } 
 
     //return results for 3 correct answers
-    if(correctAnswers > 68) {
+    if(correctAnswers === 100) {
         results.textContent = 'Hey ' + name + ', you scored ' + correctAnswers + '% on the quiz! Nailed it!';
+        results.classList.add('good-score');
     } 
 
     //remove the hidden class on the results box to reveal the results on the page
