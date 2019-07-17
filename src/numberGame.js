d2a1a59020ac53e84a4956a28072d0dbe323127d
allisonbusse <allisonbusse@gmail.com>
2019-07-17 09:24:45 -0700

    got js working totally plus more formatting

--- src/numberGame.js
+++ src/numberGame.js
@@ -4,39 +4,43 @@ import compareNumbers from './guessnumber.js';
 //Reference needed DOM elements
 const checkAnswer = document.getElementById('check-answer');
 const guess = document.getElementById('user-guess');
+const resultsSection = document.getElementById('results-section')
 const guessResponse = document.getElementById('guess-response');
 const guessesLeft = document.getElementById('guesses-left');
 
 
 //Initialization
-const correctNumber = 12;
+// Math function from MDN
+const correctNumber = Math.floor(Math.random() * 20);
 let guessCount = 4;
 
 //Event Handlers
 checkAnswer.addEventListener('click', () => {
-    
+    guessCount -= 1;
     if(compareNumbers(Number(guess.value), correctNumber) === 0) {
+        resultsSection.classList.remove('hidden');
         guessResponse.textContent = 'You got it! Way to go!';
         checkAnswer.disabled = true;
     }
     
     else if(guessCount < 1) {
+        resultsSection.classList.remove('hidden');
         guessResponse.textContent = 'Ah, so close! But now you\'re out of guesses';
         guessesLeft.textContent = '';
         checkAnswer.disabled = true;
     }
     
     else if(compareNumbers(Number(guess.value), correctNumber) === 1) {
-        guessCount -= 1;
+        resultsSection.classList.remove('hidden');
         guessResponse.textContent = 'Oooh you\'re a bit too high';
-        guessesLeft.textContent = 'You have ' + guessCount + ' guesses left.';
+        guessesLeft.textContent = 'Number of guesses left: ' + guessCount;
         
     }
 
     else if(compareNumbers(Number(guess.value), correctNumber) === -1) {
-        guessCount -= 1;
+        resultsSection.classList.remove('hidden');
         guessResponse.textContent = 'Oooh you\'re a bit too low';
-        guessesLeft.textContent = 'You have ' + guessCount + ' guesses left.';
+        guessesLeft.textContent = 'Number of guesses left: ' + guessCount;
         
     }
 
