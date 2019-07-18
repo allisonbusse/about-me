bbc3ed57fa59bebb2d1da7eb533ed60f42716243
allisonbusse <allisonbusse@gmail.com>
2019-07-17 18:13:47 -0700

    added more to js

--- src/rps-game.js
+++ src/rps-game.js
@@ -4,7 +4,7 @@ import getThrow from './get-throw.js';
 //Reference needed DOM elements
 const playGame = document.getElementById('play-game');
 const generatedImage = document.getElementById('generated-hand');
-const outcomeMessage = document.getElementById('outcome-message');
+let outcomeMessage = document.getElementById('outcome-message');
 const winCount = document.getElementById('win-count');
 const lossCount = document.getElementById('loss-count');
 
@@ -14,11 +14,22 @@ let losses = 0;
 
 //Event Handlers
 playGame.addEventListener('click', () => {
+    //run function to generate computer's throw
     const computerThrow = getThrow();
-    console.log(computerThrow);
+    
     //define the image src of the computer's throw
     generatedImage.classList.remove('invisible');
     const src = 'assets/' + computerThrow + '.png';
     generatedImage.src = src;
+
+    //get user choice from input
+    let userChoice = document.querySelector('input:checked').value;
+    
+    //compare computer throw to user choice
+    if(userChoice === computerThrow) {
+        outcomeMessage = 'Draw! Try again.';
+    }
+    
+    //tally score
 
 });    
