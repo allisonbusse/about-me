b6fe8e5f47f6f828ac8ad8d6fa3317745849a7e8
allisonbusse <allisonbusse@gmail.com>
2019-07-18 10:47:22 -0700

    adding betting feature

--- src/rps-game.js
+++ src/rps-game.js
@@ -6,12 +6,16 @@ const playGame = document.getElementById('play-game');
 const generatedImage = document.getElementById('generated-hand');
 const userImage = document.getElementById('user-image');
 let outcomeMessage = document.getElementById('outcome-message');
+const resultsSection = document.getElementById('results-section');
 const winCount = document.getElementById('win-count');
 const lossCount = document.getElementById('loss-count');
+let userBet = document.getElementById('user-bet');
+const userBalance = document.getElementById('user-balance');
 
 //Initialization
 let wins = 0;
 let losses = 0;
+let balance = 500;
 
 //Event Handlers
 playGame.addEventListener('click', () => {
@@ -20,58 +24,69 @@ playGame.addEventListener('click', () => {
     
     //get user choice from input
     let userChoice = document.querySelector('input:checked').value;
+    let bet = userBet.value;
     
     //define the image src of the computer's throw
     generatedImage.classList.remove('hidden');
-    const src = 'assets/' + computerThrow + '.png';
+    const src = 'assets/computer-' + computerThrow + '.png';
     generatedImage.src = src;
 
     //define the image src of the user's throw
     userImage.classList.remove('hidden');
-    const userSrc = 'assets/' + userChoice + '.png';
+    const userSrc = 'assets/user-' + userChoice + '.png';
     userImage.src = userSrc;
 
 
-    //remove hidden class from outcome message
-    outcomeMessage.classList.remove('hidden');
+    //remove hidden class from results section
+    resultsSection.classList.remove('hidden');
     
     //compare computer throw to user choice
     if(userChoice === computerThrow) {
         outcomeMessage.textContent = 'Draw! Try again.';
+        balance;
     }
+    
     else if(userChoice === 'rock') {
         if(computerThrow === 'scissors') {
             outcomeMessage.textContent = 'Rock crushes scissors, you win!';
             wins++;
+            balance -= -bet;
         }
         else {
             outcomeMessage.textContent = 'Paper covers rock. You lose :(';
             losses++;
+            balance -= bet;
         }
     }
     else if(userChoice === 'paper') {
         if(computerThrow === 'rock') {
             outcomeMessage.textContent = 'Paper covers rock. You win!';
             wins++;
+            balance -= -bet;
         }
         else {
             outcomeMessage.textContent = 'Scissors crush rock. You lose :(';
             losses++;
+            balance -= bet;
         }
     }
     else if(userChoice === 'scissors') {
         if(computerThrow === 'paper') {
             outcomeMessage.textContent = 'Scissors cut paper. You win!';
             wins++;
+            balance -= -bet;
         }
         else {
             outcomeMessage.textContent = 'Rock crushes paper. You lose :(';
             losses++;
+            balance -= bet;
         }
     }
+  
     
-    //tally score
+    //tally score and balance
     winCount.textContent = wins;
     lossCount.textContent = losses;
+    userBalance.textContent = balance;
 
 });    
