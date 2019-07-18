fbdb14f64d8ed21c18069c7b322dc3aa300a5bfc
allisonbusse <allisonbusse@gmail.com>
2019-07-18 09:26:29 -0700

    wrote the rest of the js

--- src/rps-game.js
+++ src/rps-game.js
@@ -4,6 +4,7 @@ import getThrow from './get-throw.js';
 //Reference needed DOM elements
 const playGame = document.getElementById('play-game');
 const generatedImage = document.getElementById('generated-hand');
+const userImage = document.getElementById('user-image');
 let outcomeMessage = document.getElementById('outcome-message');
 const winCount = document.getElementById('win-count');
 const lossCount = document.getElementById('loss-count');
@@ -17,19 +18,60 @@ playGame.addEventListener('click', () => {
     //run function to generate computer's throw
     const computerThrow = getThrow();
     
-    //define the image src of the computer's throw
-    generatedImage.classList.remove('invisible');
-    const src = 'assets/' + computerThrow + '.png';
-    generatedImage.src = src;
-
     //get user choice from input
     let userChoice = document.querySelector('input:checked').value;
     
+    //define the image src of the computer's throw
+    generatedImage.classList.remove('hidden');
+    const src = 'assets/' + computerThrow + '.png';
+    generatedImage.src = src;
+
+    //define the image src of the user's throw
+    userImage.classList.remove('hidden');
+    const userSrc = 'assets/' + userChoice + '.png';
+    userImage.src = userSrc;
+
+
+    //remove hidden class from outcome message
+    outcomeMessage.classList.remove('hidden');
+    
     //compare computer throw to user choice
     if(userChoice === computerThrow) {
-        outcomeMessage = 'Draw! Try again.';
+        outcomeMessage.textContent = 'Draw! Try again.';
+    }
+    else if(userChoice === 'rock') {
+        if(computerThrow === 'scissors') {
+            outcomeMessage.textContent = 'Rock crushes scissors, you win!';
+            wins++;
+        }
+        else {
+            outcomeMessage.textContent = 'Paper covers rock. You lose :(';
+            losses++;
+        }
+    }
+    else if(userChoice === 'paper') {
+        if(computerThrow === 'rock') {
+            outcomeMessage.textContent = 'Paper covers rock. You win!';
+            wins++;
+        }
+        else {
+            outcomeMessage.textContent = 'Scissors crush rock. You lose :(';
+            losses++;
+        }
+    }
+    else if(userChoice === 'scissors') {
+        if(computerThrow === 'paper') {
+            outcomeMessage.textContent = 'Scissors cut paper. You win!';
+            wins++;
+        }
+        else {
+            outcomeMessage.textContent = 'Rock crushes paper. You lose :(';
+            losses++;
+        }
     }
     
     //tally score
+    winCount.textContent = wins;
+    lossCount.textContent = losses;
 
 });    
